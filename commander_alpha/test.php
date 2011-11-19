c1fb0b7963ab1d12757026b20b41e7b15bf118e9
falanga.fra <falanga.fra@gmail.com>
2011-11-19 16:58:56 +0000

    ff

--- commander_alpha/test.php
+++ commander_alpha/test.php
@@ -3,5 +3,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once 'manager/DataManager.php';
+
+$username = "cecco";
+$password = "password";
+$email = "a@a.a";
+$nome = "francesco";
+$cognome = "falanga";
+$livello_cassiere = 0;
+
+$gestore_id = 1;
+
+$result = DataManager::inseririCassiere('NULL', 'NULL', 'NULL', $gestore_id, $username, $password, $email, $nome, $cognome, $livello_cassiere);
+if ($result){
+    echo "cassiere aggiunto";
+}else {
+    echo "an error occurred";
+}
+
 
 ?>

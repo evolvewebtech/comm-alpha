514cf7309abcdca22b4fd21149496079722c4a9f
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2011-11-20 19:49:14 +0000

    ff commit

---
+++ trunk/commander_alpha/manager/Config.php
@@ -0,0 +1,34 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Config
+ *
+ * @author francesco
+ */
+class Config {
+    
+    public function getConfig(){
+
+        $LOGGER_LEVEL = 100;
+        $LOGGER_FILE = '../logs/commander.log';
+
+        $config = array(
+                    'LOGGER_LEVEL' => $LOGGER_LEVEL,
+                    'LOGGER_FILE' => $LOGGER_FILE);
+
+        return $config;
+    }
+
+    public static function getInstance() {
+        static $objLog;
+        if(!isset($objLog)) {
+            $objLog = new Config();
+        }
+        return $objLog;
+    }
+}
+?>

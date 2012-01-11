400d2e4bdbaae79e313d7b0aeaab6d201ecb3a82
alessandro231 <alessandro231@gmail.com>
2012-01-11 19:31:47 +0000

    as commit

--- commander_alpha/login.php
+++ commander_alpha/login.php
@@ -3,7 +3,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-require('manager/HTML_Form.php');
+require_once 'manager/HTML_Form.php';
 
 $frm = new HTML_Form();
 

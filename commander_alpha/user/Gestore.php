9a1d2635c0c61c5704b6e01bee537d4be78ce50f
falanga.fra <falanga.fra@gmail.com>
2012-01-28 17:23:43 +0000

    ff commit

--- commander_alpha/user/Gestore.php
+++ commander_alpha/user/Gestore.php
@@ -27,7 +27,8 @@ class Gestore extends User {
 
 /*
      * -------------------------------------------------------------------------
-     * GESTIONE CASSIERI    --------------------------------------------------------
+     * GESTIONE CASSIERI    ----------------------------------------------------
+     *
      * - addCassiere()
      * - editCassiere()
      * - getCassiere()
@@ -74,14 +75,15 @@ class Gestore extends User {
     }
 
     
-    public function deleteCassiere(){
-
+    public function delCassiere($utente_registrato_id){
+        return DataManager::delCassiere($utente_registrato_id);
     }
 
 
     /*
      * -------------------------------------------------------------------------
      * GESTIONE SALA    --------------------------------------------------------
+     * 
      * - addSala()
      * - editSala()
      * - getSala()
@@ -107,7 +109,7 @@ class Gestore extends User {
      * @param <type> $id
      * @param <type> $nome
      * @param <type> $posizione
-     * @return <type>
+     * @return <bool>
      */
     public function editSala($id, $nome, $posizione){
         return DataManager::editSala($id, $nome, $posizione);
@@ -116,7 +118,7 @@ class Gestore extends User {
     /**
      *
      * @param <type> $id
-     * @return <type>
+     * @return <array>
      */
     public function getSala($id){
         $sala =  DataManager::getSala($id);
@@ -126,7 +128,7 @@ class Gestore extends User {
     /**
      *
      * @param <type> $id
-     * @return <type> 
+     * @return <bool>
      */
     public function delSala($id){
         return DataManager::delSala($id);
@@ -135,11 +137,305 @@ class Gestore extends User {
     /**
      * inserisco in un array tutte le sale presenti nel db
      *
-     * @return <type>
+     * @return <array>
      */
     public function getAllSala(){
         $allSala = DataManager::getAllSala();
         return $allSala;
     }
+
+    /*
+     * -------------------------------------------------------------------------
+     * GESTIONE TAVOLO   -------------------------------------------------------
+     * 
+     * - addTavolo()
+     * - editTavolo()
+     * - getTavolo()
+     * - delTavolo()
+     *
+     * - getAllTavolo()
+     */
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $nome
+     * @param <type> $numero
+     * @param <type> $nmax_coperti
+     * @param <type> $posizione
+     * @return <bool>
+     */
+    public function addTavolo($id,$nome,$numero,$nmax_coperti,$posizione,$sala_id){
+        return DataManager::addTavolo($id,$nome,$numero,$nmax_coperti,$posizione,$sala_id);
+    }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $nome
+     * @param <type> $numero
+     * @param <type> $nmax_coperti
+     * @param <type> $posizione
+     * @return <bool>
+     */
+    public function editTavolo($id, $nome, $numero, $nmax_coperti, $posizione,$sala_id){
+        return DataManager::addTavolo($id, $nome, $numero, $nmax_coperti, $posizione,$sala_id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function delTavolo($id){
+        return DataManager::delTavolo($id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <array>
+     */
+    public function getTavolo($id){
+        return DataManager::getTavolo($id);
+    }
+
+    /**
+     *
+     * @return <bool>
+     */
+    public function getAllTavolo(){
+        return DataManager::getAllTavolo();
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * GESTIONE ALIMENTO   -----------------------------------------------------
+     * 
+     * - addAlimento()
+     * - editAlimento()
+     * - getAlimento()
+     * - delAlimento()
+     *
+     * - getAllAlimento()
+     */
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $nome
+     * @param <type> $prezzo
+     * @param <type> $iva
+     * @param <type> $colore_bottone
+     * @param <type> $descrizione
+     * @param <type> $apeso
+     * @param <type> $path_image
+     * @param <type> $codice_prodotto
+     * @param <type> $quantita
+     * @param <type> $gestore_id
+     * @param <type> $categoria_id
+     * @param <type> $alimento_id
+     * @return <bool>
+     */
+    public function addAlimento($id, $nome, $prezzo, $iva, $colore_bottone,
+                                $descrizione, $apeso, $path_image, $codice_prodotto,
+                                $quantita, $gestore_id, $categoria_id, $alimento_id){
+        return DataManager::inserisciAlimento($id, $nome, $prezzo, $iva, $colore_bottone,
+                                              $descrizione, $apeso, $path_image, $codice_prodotto,
+                                              $quantita, $gestore_id, $categoria_id, $alimento_id);
+    }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $nome
+     * @param <type> $prezzo
+     * @param <type> $iva
+     * @param <type> $colore_bottone
+     * @param <type> $descrizione
+     * @param <type> $apeso
+     * @param <type> $path_image
+     * @param <type> $codice_prodotto
+     * @param <type> $quantita
+     * @param <type> $gestore_id
+     * @param <type> $categoria_id
+     * @param <type> $alimento_id
+     * @return <bool>
+     */
+    public function editAlimento($id, $nome, $prezzo, $iva, $colore_bottone,
+                                 $descrizione, $apeso, $path_image, $codice_prodotto,
+                                 $quantita, $gestore_id, $categoria_id, $alimento_id){
+        return DataManager::aggiornaAlimento($id, $nome, $prezzo, $iva, $colore_bottone,
+                                             $descrizione, $apeso, $path_image, $codice_prodotto,
+                                             $quantita, $gestore_id, $categoria_id, $alimento_id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function getAlimento($id){
+        return DataManager::getAlimento($id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function delAlimento($id){
+        return DataManager::cancellaAlimento($id);
+    }
+
+    /**
+     *
+     * @return <array>
+     */
+    public function getAllAlimento(){
+        return DataManager::getAllAlimento();
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * GESTIONE ALIMENTO_ESAURITO   --------------------------------------------
+     * 
+     * - addAlimentoEsaurito()
+     * - editAlimentoEsaurito()
+     * - getAlimentoEsaurito()
+     * - delAlimentoEsaurito()
+     *
+     * - getAllAlimentoEsaurito()
+     */
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $alimento_id
+     * @param <type> $data_esaurito
+     * @return <type>
+     */
+    public function addAlimentoEsaurito($id, $alimento_id, $data_esaurito){
+        return DataManager::inserisciAlimentoEsaurito($id, $alimento_id, $data_esaurito);
+    }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $alimento_id
+     * @param <type> $data_esaurito
+     * @return <type> 
+     */
+    public function editAlimentoEsaurito($id, $alimento_id, $data_esaurito){
+        return DataManager::aggiornaAlimentoEsaurito($id, $alimento_id, $data_esaurito);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function getAlimentoEsaurito($id){
+        return DataManager::getAlimentoEsaurito($id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function delAlimentoEsaurito($id){
+        return DataManager::cancellaAlimentoEsaurito($id);
+    }
+
+    /**
+     *
+     * @return <array>
+     */
+    public function getAllAlimentoEsaurito(){
+        return DataManager::getAllAlimentoEsaurito();
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * GESTIONE VARIANTE  ------------------------------------------------------
+     *
+     * - addAlimentoEsaurito()
+     * - editAlimentoEsaurito()
+     * - getAlimentoEsaurito()
+     * - delAlimentoEsaurito()
+     *
+     * - getAllAlimentoEsaurito()
+     */
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $descrizione
+     * @param <type> $prezzo
+     * @param <type> $iva
+     * @param <type> $gestore_id
+     * @return <bool>
+     */
+    public function addVariante($id, $descrizione, $prezzo, $iva, $gestore_id){
+        return DataManager::inserisciVariante($id, $descrizione, $prezzo, $iva, $gestore_id);
+    }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $descrizione
+     * @param <type> $prezzo
+     * @param <type> $iva
+     * @param <type> $gestore_id
+     * @return <bool>
+     */
+    public function editVariante($id, $descrizione, $prezzo, $iva, $gestore_id){
+        return DataManager::aggiornaVariante($id, $descrizione, $prezzo, $iva, $gestore_id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function getVariante($id){
+        return DataManager::getVariante($id);
+    }
+
+    /**
+     *
+     * @param <int> $id
+     * @return <bool>
+     */
+    public function delVariante($id){
+        return DataManager::cancellaVariante($id);
+    }
+
+    /**
+     *
+     * @return <array>
+     */
+    public function getAllAlimentoVariante(){
+        return DataManager::getAllAlimentoVariante();
+    }
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * GESTIONE CATEGORIA  ------------------------------------------------------
+     *
+     * - addAlimentoEsaurito()
+     * - editAlimentoEsaurito()
+     * - getAlimentoEsaurito()
+     * - delAlimentoEsaurito()
+     *
+     * - getAllAlimentoEsaurito()
+     */
+
+
 }
 ?>

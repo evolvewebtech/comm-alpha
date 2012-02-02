4fcb4ef64bc56389396310e613cf411eea4d8a4c
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-02-02 16:06:57 +0000

    ff commit

--- trunk/commander_alpha/login.php
+++ trunk/commander_alpha/login.php
@@ -1,3 +1,9 @@
+<link href="media/css/login.css" rel="stylesheet" type="text/css" />
+<link href="media/css/main.css" rel="stylesheet" type="text/css" />
+
+<header>
+    <h1>Commander</h1>
+</header>
 <?php
 /* 
  * To change this template, choose Tools | Templates
@@ -10,20 +16,26 @@ $frm = new HTML_Form();
 $frmLogin = $frm->startForm('login_process.php', 'post', 'LoginForm',
     array('class'=>'myFormClass',
           'onsubmit'=>'return checkBeforeSubmit(this)') );
-
+$frmLogin .= '<fieldset>';
 $frmLogin .= $frm->addLabelFor('username', 'username: ');
-$frmLogin .= $frm->addInput('text', 'username', 'ed',
-                       array('id'=>'username',
-                             'size'=>16, 'maxlength'=>15));
+$frmLogin .= $frm->HTML5addInput('text', 'username', 'ed',
+                       array('id'=>'username'),'required autofocus');
 
+$frmLogin .= '<br />';
 $frmLogin .= $frm->addLabelFor('password', 'password: ');
-$frmLogin .= $frm->addInput('password', 'password', '12345',
-                       array('id'=>'password',
-                             'size'=>16, 'maxlength'=>15));
+$frmLogin .= $frm->HTML5addInput('password', 'password', '12345',
+                       array('id'=>'password'),'required');
+$frmLogin .= '</fieldset>';
 
-$frmLogin .= $frm->addInput('submit', 'submitMyForm', 'ENTRA');
+$frmLogin .= '<fieldset class=login>';
+$frmLogin .= $frm->addInput('submit', 'submitMyForm', 'ENTRA',array('id'=>'submitLogin'));
+$frmLogin .= '</fieldset>';
 
 $frmLogin .=  $frm->endForm();
 
 echo $frmLogin;
-?>
+    ?>
+
+<footer>
+    <small>&copy; commander</small>
+</footer>

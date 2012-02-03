507f1742b085e9dcc68f23a4fa9183dbbe7c64b3
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-02-03 10:10:14 +0000

    ff commit

--- trunk/commander_alpha/test.php
+++ trunk/commander_alpha/test.php
@@ -1,6 +1,54 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    require_once dirname(__FILE__).'/manager/HTTPSession.php';
+    $objSession = new HTTPSession();
+?>
+<link rel="stylesheet" href="media/css/main.css" type="text/css" media="screen" />
+<script type="text/javascript" src="media/js/jquery.js"></script>
+<style type="text/css">
+    /*Demo Styles*/
+    body{ background:#e9e8e4 url('img/bg-black.png'); }
+    #content{ width:920px; margin:20px auto; padding:10px 30px; }
+    .clearfix{ display: block; height: 0; clear: both; visibility: hidden; }
+    .details{ margin:15px 20px; }
+    h4{ font:300 16px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        line-height:160%; letter-spacing:0.15em; color:#fff;
+        text-shadow:1px 1px 0 rgb(0,0,0); }
+    p{ font:300 12px 'Lucida Grande', Tahoma, Verdana, sans-serif;
+       color:#aaa; text-shadow:1px 1px 0 rgb(0,0,0);}
+    a{ text-decoration:none; }
+
+</style>
+<div id="content">
+    <?php
+    if($objSession->IsLoggedIn()){
+        $objUser = $objSession->GetUserObject();
+        $gestore = $objUser[0];
+        if(get_class($gestore) == 'Gestore') {
+    ?>
+    <h1>Configura il ristorante
+        <small>
+            <a style="color:#fff;text-align: right; font-size: 14px; float: right;" href="logout.php">
+                <-- menu principale
+            </a>
+        </small>
+    </h1>
+
+
+        <h4 style="margin-left: 10px;">
+            <a style="color:#fff;" href="logout.php">esci</a> |
+            <a style="color:#fff;" href="license.php">credit</a>
+        </h4>
+</div>
+<?php
+        }//gestore
+        else{
+            echo "<h4>Non possiedi i permessi necessari per visualizzare questa pagina.
+                Contatta l'amministratore.</h4>";
+        }
+    }//isLoggedin
+    else {
+       echo '<h4 style="margin-left: 10px;">Sessione scaduta o autenticazione errata.
+                <a style="color:#fff;" href="logout.php"> --> LOGIN</a>
+            </h4>';
+    }
 ?>

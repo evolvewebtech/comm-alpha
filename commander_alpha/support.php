fef42535885f0fee0a5c21bc93ceb7951681693c
falanga.fra <falanga.fra@gmail.com>
2012-02-03 15:24:55 +0000

    ff commit

---
+++ commander_alpha/support.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<link href="media/css/main.css" rel="stylesheet" type="text/css" />
+<style>
+    * {color:#fff}
+</style>
+<div id="content">
+    <h1>SUPPORTO COMMANDER</h1>
+    <h2>SUPPORTO OFF-LINE</h2>
+    <h5>SALA e TAVOLI</h5>
+    <ul>
+        <li>Aggiungere una sala</li>
+        <li>Modificare una sala</li>
+        <li>Eliminare una sala</li>
+    </ul>
+    <h5>CASSIERI</h5>
+    <ul>
+        <li>Aggiiungere un cassiere</li>
+        <li>Modificare un cassiere</li>
+        <li>Eliminare un cassiere</li>
+    </ul>
+    <h5>ALIMENTI E VARIANTI</h5>
+    <ul>
+        <li>Aggiungere un alimento</li>
+        <li>Modificare un alimento</li>
+        <li>Eliminare un alimento</li>
+        <li>Aggiungere una variante ad un alimento</li>
+        <li>Modificare una variante ad un alimento</li>
+        <li>Eliminare una variante ad un alimento</li>
+    </ul>
+    <h5>MENU'</h5>
+        <li>Aggiungere una menu fisso</li>
+        <li>Modificare una menu fisso</li>
+        <li>Eliminare una menu fisso</li>    
+        <li>Aggiungere un alimento ad un menu fisso</li>
+        <li>Eliminare un alimento da un menu fisso</li>
+    <h5>STAMPANTI</h5>
+    <ul>
+        <li>Gestisci le stampanti</li>
+    </ul>
+    <h5>STATISTICHE</h5>
+    <ul>
+        <li>Visualizza le statistiche</li>
+    </ul>
+    
+    <h2>SUPPORTO ON-LINE</h2>
+    <ul>
+        <li><a href="#">www.commander.it</a></li>
+        <li><a href="mailto:#">support@commander.it</a></li>
+    </ul>
+
+    <h2>SUPPORTO TEEFONICO</h2>
+    <ul>
+        <li>Alessandro: </li>
+        <li>Francesco: +39 340 27 39 492</li>
+    </ul>
+
+    <h6>&copy; commander, some rights reserver.</h6><br />
+    <a style="text-decoration: none; font-size: 12px;" href="amministrazione.php"> <- torna</a>
+</div>

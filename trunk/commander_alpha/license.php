48de5505138fa126de76d1d6bdfa2fc3e606edf8
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-02-03 09:16:24 +0000

    ff commit

---
+++ trunk/commander_alpha/license.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<link href="media/css/main.css" rel="stylesheet" type="text/css" />
+<style>
+    * {color:#fff}
+</style>
+<div id="content">
+    <h1>Termini e condizioni d'uso</h1>
+    <p>
+        
+    </p>
+    <h4>&copy; commander, some rights reserver.</h4><br />
+    <a style="text-decoration: none; font-size: 12px;" href="amministrazione.php"> <- torna</a>
+</div>

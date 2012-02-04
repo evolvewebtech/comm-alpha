0eeacf60c2eb88bfcab188f306b59b2244c2b73b
alessandro231@gmail.com <alessandro231@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-02-04 10:55:11 +0000

    as commit

--- trunk/commander_alpha/ordinazione.php
+++ trunk/commander_alpha/ordinazione.php
@@ -1,15 +1,41 @@
-<?php
-require_once dirname(__FILE__).'/manager/HTTPSession.php';
 
-$objSession = new HTTPSession();
-$sess_id = $objSession->GetSessionIdentifier();
-$log_in = $objSession->IsLoggedIn();
-?>
-<a href="logout.php">logout</a>
-<h1>Gestione ordini</h1>
-<br />ISLOGGEDIN:[ <?= var_dump($log_in) ?> ]
-<br />SESSION ID: [ <?= $sess_id ?> ]
-<br />variabile di sessione UTENTE REGISTRATO ID: [ <?= $objSession->UTENTE_REGISTRATO_ID ?> ]
-<br />variabile di sessione RUOLO: [ <?= $objSession->RUOLO ?> ]
-<br />variabile di sessione LIVELLO_CASSIERE: [ <?= $objSession->LIVELLO_CASSIERE ?> ]
-<br />variabile di sessione GESTORE ASSOCIATO ID: [ <?= $objSession->GESTORE_ID ?> ]
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Commander</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        
+        <?php      
+            /**
+             * Verifica se l'utente è loggato come "Cassiere"
+             */       
+            require_once dirname(__FILE__).'/manager/HTTPSession.php';
+
+            $objSession = new HTTPSession();
+            $sess_id = $objSession->GetSessionIdentifier();
+            $log_in = $objSession->IsLoggedIn();
+            $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
+        
+            if($objSession->IsLoggedIn()){
+                $objUser = $objSession->GetUserObject();
+                $gestore = $objUser[0];
+                if(get_class($gestore) != 'Cassiere') {
+                    //Utente senza diritti
+                    header("location: login.php");
+                }                
+            }
+            else {
+                //Utente non loggato
+                header("location: login.php");
+            }
+        ?>
+        
+    </head>
+    <body>
+        <div>
+            
+        </div>
+    </body>
+</html>
+

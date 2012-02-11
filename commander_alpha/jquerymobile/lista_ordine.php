23e6985c9358bfa741f6e1993590fd248d0c7f9a
alessandro231 <alessandro231@gmail.com>
2012-02-11 18:57:37 +0000

    as commit

--- commander_alpha/jquerymobile/lista_ordine.php
+++ commander_alpha/jquerymobile/lista_ordine.php
@@ -1,10 +1,20 @@
 
+<section id="options" class="clearfix">
+    <div class="option-combo">
+        <ul id="sort" class="option-set4 clearfix" data-option-key="sortBy">
+            <li><a href="#categorie" data-option-value="original-order" class="selected">categorie</a></li>
+            <li><a href="#alphabetical" data-option-value="alphabetical">alphabetical</a></li>
+        </ul>
+    </div>
+</section>
+
 <div id="container2" class="super-list variable-sizes clearfix"></div>
 
-<div id="totale" class="element_list">;
-<h2 class="name">Totale:</h2>
-<h2 class="prezzo">0 €</h2>
+<div id="totale" class="element_list">
+    <h2 class="name">Totale:</h2>
+    <h2 class="prezzo">0 €</h2>
 </div>
+
 
 <?php
 

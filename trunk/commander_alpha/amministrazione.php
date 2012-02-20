62934f6d194ea10ae96fa346680f32a2e2ff0376
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-02-20 20:18:08 +0000

    ff commit: sale/tavoli/cassieri quasi conclusi.

--- trunk/commander_alpha/amministrazione.php
+++ trunk/commander_alpha/amministrazione.php
@@ -54,6 +54,26 @@ $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
                         hover_y		:	'300px'		//Vertical position on hover
                 });
 
+                //effetto slide al premere di "gestisci alimenti"
+                $(".accordion .admin-page").eq(0).show();
+
+                $(".accordion .accordion-alimento").click(function(){
+                    $('#amministrazione').slideToggle('slow', function() {
+                        // Animation complete.
+                        });
+                    $('#alimento').slideToggle('slow', function() {
+                        // Animation complete.
+                        });
+                });
+                $(".accordion .accordion-back").click(function(){
+                    $('#alimento').slideToggle('slow', function() {
+                        // Animation complete.
+                        });
+                    $('#amministrazione').slideToggle('slow', function() {
+                        // Animation complete.
+                        });
+                });
+
     });
 
 </script>
@@ -69,7 +89,19 @@ $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
     p{ font:300 12px 'Lucida Grande', Tahoma, Verdana, sans-serif;
        color:#aaa; text-shadow:1px 1px 0 rgb(0,0,0);}
     a{ text-decoration:none; }
+    
+    .accordion .admin-page{
+            display: none;
+        }
 
+    .accordion2 p {
+            background: #f7f7f7;
+            margin: 0;
+            padding: 10px 15px 20px;
+            border-left: solid 1px #c4c4c4;
+            border-right: solid 1px #c4c4c4;
+            display: none;
+    }
 </style>
 <div id="content">
     <?php
@@ -78,94 +110,144 @@ $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
         $gestore = $objUser[0];
         if(get_class($gestore) == 'Gestore') {
     ?>
-        <!--Fade-->
-        <div class="mosaic-block fade">
-            <a href="amministrazioneSale.php" class="mosaic-overlay">
-                    <div class="details">
-                            <h4>CONFIGURA IL RISTORANTE</h4>
-                            <p>Crea le sale, aggiungi i tavoli e rinominali!</p>
-                    </div>
-            </a>
-            <div style="background-color:#fff" class="mosaic-backdrop">
-                <img alt="ristorante" src="img/restaurant.png"/>
-            </div>
-        </div>
-
-        <!--Bar-->
-        <div class="mosaic-block bar">
-            <a href="amministrazioneCassieri.php" class="mosaic-overlay">
-                    <div class="details">
-                            <h4>GESTISCI I CASSIERI</h4>
-                            <p>Aggiungi nuovi cassieri al tuo ristorante,
-                                modifica i dati di un cassiere gi&agrave;
-                                esistente o elimanane l'account.</p>
-                    </div>
-            </a>
-            <div style="background-color:#fff" class="mosaic-backdrop">
-                <img src="img/waiter.png"/>
-            </div>
-        </div>
-
-        <!--Bar 2-->
-        <div class="mosaic-block bar2">
-            <a href="#" target="_blank" class="mosaic-overlay">
-                <div class="details">
-                        <h4>GESTISCI GLI ALIMENTI</h4><br/>
-                        <p>
-                            Iserisci tutte le categorie (primi - secondi - contorni...),
-                            iserisci nuovi alimentie ed iserisci le varianti ai tuoi piatti!
-                        </p>
+     <div class="accordion">
+        <div id="amministrazione" class="admin-page">
+            <div class="mosaic-block fade">
+                <a href="amministrazioneSale.php" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>CONFIGURA IL RISTORANTE</h4>
+                                <p>Crea le sale, aggiungi i tavoli e rinominali!</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img alt="ristorante" src="img/restaurant.png"/>
                 </div>
-            </a>
-            <div style="background-color:#fff" class="mosaic-backdrop">
-                <img src="img/food.png"/>
             </div>
-        </div>
 
-        <!--Bar 3-->
-        <div class="mosaic-block bar3">
-            <a href="#" target="_blank" class="mosaic-overlay">
-                    <div class="details">
-                            <h4>GESTISCI I MENU</h4>
-                            <p>Crea e personalizza i tuoi men&ugrave.</p>
-                    </div>
-            </a>
-            <div style="background-color:#fff" class="mosaic-backdrop">
-                <img style="padding:60px"src="img/menu.png"/>
-            </div>
-        </div>
-
-        <!--Cover-->
-        <div class="mosaic-block cover">
-            <div class="mosaic-overlay" style="background-color:#fff">
-                <img src="img/printer.png"/>
-            </div>
-            <a href="#" target="_blank" class="mosaic-backdrop">
-                <div class="details">
-                        <h4>GESTISCI LE STAMPANTI</h4>
-                        <p>Aggiungi nuove stampanti e associale alle casse del tuo ristorante!</p>
+            <!--Bar-->
+            <div class="mosaic-block bar">
+                <a href="amministrazioneCassieri.php" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>GESTISCI I CASSIERI</h4>
+                                <p>Aggiungi nuovi cassieri al tuo ristorante,
+                                    modifica i dati di un cassiere gi&agrave;
+                                    esistente o elimanane l'account.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img src="img/waiter.png"/>
                 </div>
-            </a>
-        </div>
+            </div>
 
-        <!--Cover 2-->
-        <div class="mosaic-block cover2">
-            <div style="background-color:#fff" class="mosaic-overlay"><img src="img/stat.png"/></div>
-            <a href="#" target="_blank" class="mosaic-backdrop">
+            <!--Bar 2-->
+            <div class="mosaic-block bar2">
+                <a href="#" class="mosaic-overlay accordion-alimento">
                     <div class="details">
-                            <h4>STATISTICHE</h4>
-                            <p>Qui trovi tutte le informazioni che ti interessano.</p>
+                            <h4>GESTISCI GLI ALIMENTI</h4><br/>
+                            <p>
+                                Iserisci tutte le categorie (primi - secondi - contorni...),
+                                iserisci nuovi alimentie ed iserisci le varianti ai tuoi piatti!
+                            </p>
                     </div>
-            </a>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img src="img/food.png"/>
+                </div>
+            </div>
+
+            <!--Bar 3-->
+            <div class="mosaic-block bar3">
+                <a href="#" target="_blank" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>GESTISCI I MENU</h4>
+                                <p>Crea e personalizza i tuoi men&ugrave.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img style="padding:60px"src="img/menu.png"/>
+                </div>
+            </div>
+
+            <!--Cover-->
+            <div class="mosaic-block cover">
+                <div class="mosaic-overlay" style="background-color:#fff">
+                    <img src="img/printer.png"/>
+                </div>
+                <a href="#" target="_blank" class="mosaic-backdrop">
+                    <div class="details">
+                            <h4>GESTISCI LE STAMPANTI</h4>
+                            <p>Aggiungi nuove stampanti e associale alle casse del tuo ristorante!</p>
+                    </div>
+                </a>
+            </div>
+
+            <!--Cover 2-->
+            <div class="mosaic-block cover2">
+                <div style="background-color:#fff" class="mosaic-overlay"><img src="img/stat.png"/></div>
+                <a href="#" target="_blank" class="mosaic-backdrop">
+                        <div class="details">
+                                <h4>STATISTICHE</h4>
+                                <p>Qui trovi tutte le informazioni che ti interessano.</p>
+                        </div>
+                </a>
+            </div>
         </div>
 
-        <div class="clearfix"></div>
-        <h4 style="margin-left: 10px;">
-            <a style="color:#fff;" href="logout.php">esci</a> |
-            <a style="color:#fff;" href="support.php">supporto</a> |
-            <a style="color:#fff;" href="license.php">credit</a>
-        </h4>
-</div>
+        <div id="alimento" class="admin-page">
+            <div class="mosaic-block fade">
+                <a href="#" class="mosaic-overlay accordion-back">
+                        <div class="details">
+                                <h4><- INDIETRO</h4>
+                                <p>Torna al pannello amministrazione.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img alt="ristorante" src="img/undo.png"/>
+                </div>
+            </div>
+            <div class="mosaic-block fade">
+                <a href="#" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>CATEGORIE</h4>
+                                <p>Visualizza le categorie, inseriscine di nuove o modifica quelle esistenti.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img alt="ristorante" src="img/categories.png"/>
+                </div>
+            </div>
+            <div class="mosaic-block fade">
+                <a href="#" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>ALIMENTI</h4>
+                                <p>Visualizza gli alimenti, inseriscine di nuovi o modifica quelli esistenti.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img alt="ristorante" src="img/beef.png"/>
+                </div>
+            </div>
+            <div class="mosaic-block fade">
+                <a href="#" class="mosaic-overlay">
+                        <div class="details">
+                                <h4>VARIANTI</h4>
+                                <p>Visualizza le varianti, inseriscine di nuove o modifica quelle esistenti.</p>
+                        </div>
+                </a>
+                <div style="background-color:#fff" class="mosaic-backdrop">
+                    <img alt="ristorante" src="img/add.png"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="clearfix"></div>
+    <h4 style="margin-left: 10px;">
+        <a style="color:#fff;" href="logout.php">esci</a> |
+        <a style="color:#fff;" href="support.php">supporto</a> |
+        <a style="color:#fff;" href="license.php">credit</a>
+    </h4>
+</div><!-- end content -->
 
 <!--
 <br />ISLOGGEDIN:[ <?//= var_dump($log_in) ?> ]

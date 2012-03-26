6499691e088126e702121c3c8141954c1e8ff499
alessandro231 <alessandro231@gmail.com>
2012-03-26 20:38:47 +0000

    as commit

--- commander_alpha/jquerymobile/index.php
+++ commander_alpha/jquerymobile/index.php
@@ -9,6 +9,8 @@
     <script src="js/jquery.mobile-1.0.1.min.js"></script>
     <script>
         //Dichiarazione variabili globali;
+        var numTavolo = 0;
+        var numCoperti = 0;
         var arrAlim = new Array();
         var arrMenu = new Array();
         var arrMenuSel = new Array();
@@ -17,7 +19,7 @@
         var mem_index = -1;
         var mem_ord_type = "cat";
     </script>
-    <script type="text/javascript" src="menu.js"></script>
+    <script type="text/javascript" src="menu.js"></script>z
 </head>
 <body>
     
@@ -28,7 +30,7 @@
         </div>
         <div data-role="content">
             <div class="scelta_op">
-                <div class="button_opz">
+                <div class="button_opz" >
                     <a href="#tavoli"><img src="images/symbol_add.png"/></a>
                 </div>
                 <div class="button_opz">
@@ -48,13 +50,23 @@
             <h1>Scelta tavolo</h1>
         </div>
         <div data-role="content">
-            <label for="basic">Numero tavolo:</label>
-            <input type="text" name="name" id="basic" value=""  />
-            <form>
-                <label for="slider-0">Numero coperti:</label>
-                <input type="range" name="slider" id="slider-0" value="1" min="0" max="100"  />
-            </form>
-            <a href="#ordine" data-role="button" data-icon="grid" class="ui-btn-right">Ordine</a>
+            <div class="ui-grid-b">
+                <div class="ui-block-a"></div>
+        	<div class="ui-block-b">
+                    <h1></h1>
+                    <label for="basic">Inserire il numero del tavolo:</label>
+                    <input type="text" name="name" id="basic" value="" placeholder="Numero tavolo" />
+                    <h1></h1>
+                    <form>
+                        <label for="slider-0">Inserire il numero di coperti:</label>
+                        <input type="range" name="slider" id="slider-0" value="1" min="0" max="100"  />
+                    </form>
+                    <h1></h1>
+                    <a id="sel-table" href="#ordine" data-role="button" data-icon="grid" class="ui-btn-right">Inserimento ordine</a>
+                    <a href="#home" data-role="button" data-icon="delete" class="ui-btn-right">Annulla ordine</a>
+                </div>
+                <div class="ui-block-c"></div>
+            </div><!-- /grid-b -->
         </div>
     </div>
     
@@ -95,6 +107,17 @@
         <div data-role="content">
             <a href="#ordine" data-role="button" data-icon="check" class="ui-btn-right canc-all-conf">Cancella tutto</a>
             <a href="#ordine" data-role="button" data-icon="delete" class="ui-btn-right canc-ann">Annulla</a>
+        </div>
+    </div>
+    
+    <!-- DIALOG CONFERMA CANCELLA ORDINE -->
+    <div data-role="page" id="diag-conf-canc-ord">
+        <div data-role="header">
+            <h1>Annullare l'ordine?</h1>
+        </div>
+        <div data-role="content">
+            <a id="canc-ord" href="#home" data-role="button" data-icon="check" class="ui-btn-right canc-all-conf">Sì</a>
+            <a href="#ordine" data-role="button" data-icon="delete" class="ui-btn-right canc-ann">No</a>
         </div>
     </div>
 </body>

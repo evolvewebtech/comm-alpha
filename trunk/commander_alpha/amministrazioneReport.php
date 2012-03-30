e621507a9851a42e4229cefc00e639fef376a2a4
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-03-30 13:58:14 +0000

    ff commit:

---
+++ trunk/commander_alpha/amministrazioneReport.php
@@ -0,0 +1,74 @@
+<?php
+    require_once dirname(__FILE__).'/manager/HTTPSession.php';
+    $objSession = new HTTPSession();
+?>
+<link rel="stylesheet" href="media/css/main.css" type="text/css" media="screen" />
+<script type="text/javascript" src="media/js/jquery-1.7.1.min.js"></script>
+
+<!-- jqPlot -->
+<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="media/js/excanvas.js"></script><![endif]-->
+<script language="javascript" type="text/javascript" src="media/js/jquery.jqplot.min.js"></script>
+<link rel="stylesheet" type="text/css" href="media/css/jquery.jqplot.css" />
+
+<style type="text/css">
+    /*
+     * foglio di stile per la pagina corrente
+     *
+     */
+    .clearfix{ display: block; height: 0; clear: both; visibility: hidden; }
+    h4{ font:300 16px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        line-height:160%; letter-spacing:0.15em; color:#fff;
+        text-shadow:1px 1px 0 rgb(0,0,0); }
+    p{ font:300 12px 'Lucida Grande', Tahoma, Verdana, sans-serif;
+       color:#000;}
+    a{ text-decoration:none; }
+</style>
+
+<div id="content">
+    <?php
+    if($objSession->IsLoggedIn()){
+        $objUser = $objSession->GetUserObject();
+        $gestore = $objUser[0];
+        if(get_class($gestore) == 'Gestore') {
+
+           $gestore_id = $gestore->id;
+           $utente_registrato_id = $gestore->utente_registrato_id;
+
+           $data_cassieri = DataManager::getTuttiCassieri($gestore_id);
+           $numero_cassieri = count($data_cassieri);
+
+//           echo '<p style="background-color:white">'.$numero_tavolo.'</p>';
+    ?>
+    <h1>Business Intelligence
+        <small style="color:#fff;text-align: right; font-size: 12px; float: right;">
+            Sei qui: <a style="color:#fff; font-size: 12px;" href="amministrazione.php">menu principale</a> >
+                     <a style="color:#fff; font-size: 14px;" href="amministrazioneReport.php"><b>Business Intelligence</b></a>
+        </small>
+    </h1>
+
+
+
+
+
+        <h4 style="margin-left: 10px; float:left; width: 920px;">
+            <a style="color:#fff;" href="logout.php">esci</a> |
+            <a style="color:#fff;" href="support.php">supporto</a> |
+            <a style="color:#fff;" href="license.php">credit</a>
+        </h4>
+</div><!-- end content -->
+
+        <!-- DEBUG -->
+        <div id="debug" style="width: 920px;float:left; margin-top: 30px;color:white; font-size: 10px;">DEBUG:</div>
+<?php
+        }//gestore
+        else{
+            echo "<h4>Non possiedi i permessi necessari per visualizzare questa pagina.
+                Contatta l'amministratore.</h4>";
+        }
+    }//isLoggedin
+    else {
+       echo '<h4 style="margin-left: 10px;">Sessione scaduta o autenticazione errata.
+                <br /><a style="color:#fff;" href="logout.php"> <-- LOGIN</a>
+            </h4>';
+    }
+?>

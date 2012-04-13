863cf56755420c1bfff5d2f8ebe6bd93da9c6b05
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-04-13 00:37:39 +0000

    ff commit:

--- trunk/commander_alpha/test.php
+++ trunk/commander_alpha/test.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" href="media/css/mosaic.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="media/css/main.css" type="text/css" media="screen" />
 <script type="text/javascript" src="media/js/jquery-1.7.1.min.js"></script>
+
+
 <style type="text/css">
     /*Demo Styles*/
-    body{ background:#e9e8e4 url('img/bg-black.png'); }
+    body{ background:#e9e8e4 url('img/bg-black.png'); color: #ffffff; }
     #content{ width:920px; margin:20px auto; padding:10px 30px; }
     .clearfix{ display: block; height: 0; clear: both; visibility: hidden; }
     .details{ margin:15px 20px; }
@@ -13,12 +15,62 @@
     p{ font:300 12px 'Lucida Grande', Tahoma, Verdana, sans-serif;
        color:#aaa; text-shadow:1px 1px 0 rgb(0,0,0);}
     a{ text-decoration:none; }
+    table{
+        background-color: #fff;
+        color: #000;
+        border: #000 1px solid;
+    }
+    tr {
+        
+    }
+    td {
+        /*border: #000 1px solid;*/
+        padding: 1px;
+    }
+    td.third {
+        /*border: #000 1px solid;*/
+        padding-left: 5px;
+    }
+
+    td.second {
+        border-right: 2px #000 dotted;
+        padding-right: 5px;
+}
 </style>
+
+<table>
+    <tr>
+        <td>N.</td>
+        <td class="second">AB0001</td>
+
+        <td class="third">NUMERO</td>
+        <td>NOME</td>
+        <td>CREDITO</td>
+    </tr>
+    <tr>
+        <td>NOME</td>
+        <td class="second">Band Titti</td>
+
+        <td class="third">AB0001</td>
+        <td>Band Titti</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <td>CRED.</td>
+        <td class="second">5</td>
+
+        <td class="third" colspan="3" style="font-size:12px; text-align: right;"><small><i><b>Sagra di S.Anna e Gioacchino</b></i></small></td>
+    </tr>
+</table>
 <?php
-/*
 require_once dirname(__FILE__).'/manager/HTTPSession.php';
-$objSession = new HTTPSession();
-    
+require_once dirname(__FILE__).'/manager/Datamanager.php';
+
+require_once dirname(__FILE__). '/lib/tcpdf/config/lang/eng.php';
+require_once dirname(__FILE__). '/lib/tcpdf/tcpdf.php' ;
+
+
+/*
 if ($objSession->IsLoggedIn()){
 
 

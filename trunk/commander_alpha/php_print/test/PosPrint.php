562b609e17e4fa464cf3d0df9dbf37f18554211b
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-04-25 15:00:02 +0000

    ff commit:

---
+++ trunk/commander_alpha/php_print/test/PosPrint.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Classe per l'invio di una stampa
+ *
+ *@author Sarzina - Falanga
+ *
+ */
+class PosPrint {
+
+    /**
+     *
+     * @param <type> $ip
+     * @param <type> $out
+     * @return <type>
+     */
+    public static function comm_print($ip_address, $out) {
+
+        $fp = fsockopen($ip_address, 9100, $errno, $errstr, 10);
+        if (!$fp) {
+            return "$errstr ($errno)";
+        } else {
+            fwrite($fp, $out);
+            fclose($fp);
+            return true;
+        }
+
+    }
+
+
+}
+
+?>

b26aa3df852b025872fb22d6cfdfc2e42c4d6904
alessandro231@gmail.com <alessandro231@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-05-01 19:11:16 +0000

    as commit

--- trunk/commander_alpha/jquerymobile/index.php
+++ trunk/commander_alpha/jquerymobile/index.php
@@ -146,10 +146,20 @@
                 <h1></h1>
                 <form>
                     <label for="slider-0">Inserire il numero di coperti:</label>
-                    <input type="range" name="slider" id="slider-0" value="1" min="0" max="50"  />
+                    <input type="number" name="slider" id="slider-0" value="1" />
+                    <!-- <input type="range" name="slider" id="slider-0" value="1" min="0" max="50"  /> -->
+                    <div class="ui-grid-a">
+                    	<div class="ui-block-a">
+                            <a data-role="button" data-icon="plus" data-iconpos="top" class="ui-btn-right cop-plus"></a>
+                        </div>
+                        <div class="ui-block-b">
+                            <a data-role="button" data-icon="minus" data-iconpos="top" class="ui-btn-right cop-min"></a>
+                        </div>
+                    </div><!-- /grid-a -->    
                 </form>
                 <h1></h1>
                 <a id="sel-table" href="#ordine" data-role="button" data-icon="grid" class="ui-btn-right ui-disabled">Inserimento ordine</a>
+                <h3></h3>
                 <a href="#home" data-role="button" data-icon="delete" class="ui-btn-right">Annulla ordine</a>
             </div>
             <script>
@@ -168,6 +178,20 @@
                     }
                     else $('#sel-table').removeClass('ui-disabled');
                 }
+                
+                $('.cop-plus').live("click", function() {
+                    var temp = document.getElementById('slider-0').value;
+                    if (temp < 50) {
+                        document.getElementById('slider-0').value = parseInt(temp) + 1;
+                    }
+                });
+                
+                $('.cop-min').live("click", function() {
+                    var temp = document.getElementById('slider-0').value;
+                    if (temp > 0) {
+                        document.getElementById('slider-0').value = parseInt(temp) - 1;
+                    }
+                });
             </script>
         </div>
     </div>

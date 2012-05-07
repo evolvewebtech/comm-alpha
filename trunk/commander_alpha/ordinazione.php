ebeff1d4a52d1d0294dbfa81d655815c0277a634
falanga.fra@gmail.com <falanga.fra@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-05-07 21:01:12 +0000

    ff commit:

--- trunk/commander_alpha/ordinazione.php
+++ trunk/commander_alpha/ordinazione.php
@@ -1,41 +1,31 @@
+<?php      
+    /**
+     * Verifica se l'utente è loggato come "Cassiere"
+     */
+    require_once dirname(__FILE__).'/manager/HTTPSession.php';
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Commander</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
-        
-        <?php      
-            /**
-             * Verifica se l'utente è loggato come "Cassiere"
-             */       
-            require_once dirname(__FILE__).'/manager/HTTPSession.php';
+    $objSession = new HTTPSession();
+    $sess_id = $objSession->GetSessionIdentifier();
+    $log_in = $objSession->IsLoggedIn();
+    $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
 
-            $objSession = new HTTPSession();
-            $sess_id = $objSession->GetSessionIdentifier();
-            $log_in = $objSession->IsLoggedIn();
-            $utente_registrato = $objSession->__get('UTENTE_REGISTRATO_ID');
+    if($objSession->IsLoggedIn()){
+        $objUser = $objSession->GetUserObject();
+        $gestore = $objUser[0];
+        if(get_class($gestore) != 'Cassiere') {
+            //Utente senza diritti
+            header("location: login.php");
+        }
+ ?>
+<!-- content -->
+<div id="content">
+    
+</div>
+<?php
+        }//l'utente non è un cassiere
+    else {
+        //Utente non loggato
+        header("location: login.php");
+    }//l'utente non è loggato
+?>
         
-            if($objSession->IsLoggedIn()){
-                $objUser = $objSession->GetUserObject();
-                $gestore = $objUser[0];
-                if(get_class($gestore) != 'Cassiere') {
-                    //Utente senza diritti
-                    header("location: login.php");
-                }                
-            }
-            else {
-                //Utente non loggato
-                header("location: login.php");
-            }
-        ?>
-        
-    </head>
-    <body>
-        <div>
-            
-        </div>
-    </body>
-</html>
-

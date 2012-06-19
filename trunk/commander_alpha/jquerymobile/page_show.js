fd5a4ff1df510157d0329254d209d97bf09565a6
alessandro231@gmail.com <alessandro231@gmail.com@07f86107-ee20-76eb-2e17-c723b25378f5>
2012-06-19 20:47:47 +0000

    as commit

--- trunk/commander_alpha/jquerymobile/page_show.js
+++ trunk/commander_alpha/jquerymobile/page_show.js
@@ -1,9 +1,87 @@
 
 /*
+ * Inizializzazione Comander
+ *
+ */
+$(function(){
+    //Aggiornamento livelli e sconti cassiere
+    $.ajax({
+        type : "POST",
+        data: '',
+        url: "livelli.php",
+        dataType: 'json',
+        cache: false,
+        success: onLivelliSuccess,
+        error: onLivelliError
+    });
+});
+
+
+/**
+ * Richiesta Ajax completata con successo
+ *
+ */
+function onLivelliSuccess(data, status) { 
+
+    //Verifica se utente loggato
+    if ( !logged(data['err']) ) return;
+
+    //alert("Successo lettura Livelli da database con Ajax!");  
+
+    document.getElementById('user01').innerHTML = 'Cameriere: ' + data['cassiere'];
+
+    for(var i=0; i<data['livelli'].length; i++) {
+        livelli.push(data['livelli'][i]);
+    }
+
+    //Verifica se cassiere abilitato per prenotazione al tavolo
+    if (livelli[1] != 2) {
+        cassa_fissa = true;
+        //nascosto pulsante "Indietro" in pagina "ordine"
+        //document.getElementById('#pg-ord-back').style.display='none';
+    }
+
+    //Creazione pulsanti selezione sconto
+    var str = '';
+    for(var j=0; j<data['sconti'].length; j++) {
+        sconti.push(data['sconti'][j]);    
+        str = str + '<a href="#'+data['sconti'][j]+'" data-role="button" class="ui-link-inherit cl-sconto">';
+        str = str + '<div class="name" style="font-size: 150%">Sconto '+data['sconti'][j]+'%</div>';
+        str = str + '</a>';        
+    }
+
+    str = str + '<a href="#0" data-role="button" class="ui-link-inherit cl-sconto">';
+    str = str + '<div class="name" style="font-size: 150%">Nessuno sconto</div>';
+    str = str + '</a>';  
+
+    document.getElementById('diag-sconto-bt').innerHTML = str;
+
+    if (data['sconti'].length > 0) {
+        document.getElementById('diag-sconto-text').style.display = 'none';
+        document.getElementById('diag-sconto-bt').style.display = 'inline';
+    }
+    else {
+        document.getElementById('diag-sconto-text').style.display = 'inline';
+        document.getElementById('diag-sconto-bt').style.display = 'none';
+    }
+}
+
+
+/**
+ * Errore richiesta Ajax
+ *
+ */
+function onLivelliError(data, status) { 
+    alert("Errore Ajax Livelli " + data['err']);
+}
+
+
+/*
  * PAGINA "HOME"
  */
 $("#home").live('pageshow', function() {
     homePageShow();
+    resetTavolo();
 });
 
 
